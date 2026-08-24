30fb4faf249ef03880eef9913211a4f394f737c4
Ashmit Dixit <ashmitdixit8@gmail.com>
2026-08-24 22:52:37 +0530

    Time: 0 ms (100.00%) | Memory: 80.5 MB (22.26%) - LeetSync

--- 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.java
+++ 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.java
@@ -7,14 +7,18 @@ class Solution {
         //     j++;
         // }
         // return i;
-        int i=0;
-        int j=arr.length-1;
-        while(i<j){
-            int mid=(i+j)/2;
-            if(arr[mid]<arr[mid+1]) i=mid+1;
-            else{
-                j=mid;
+        int low=0;
+        int high=arr.length-1;
+        int ans=-1;
+        while(low<high){
+            int mid=(low+high)/2;
+            if(arr[mid]<arr[mid+1]){
+                low=mid+1;
             }
-    }
-    return i;
+            else{
+                high=mid;
+                ans=mid;
+            }
+        }
+        return ans;
 }}
